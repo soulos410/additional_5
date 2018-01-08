cf1385ce47b9cd44ef34e8385e90ee6e0c6aef45
Siarhei <solimr111@mail.ru>
2018-01-09 02:44:00 +0300

    finally completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  for (let i of str) {
+  	for (let j of bracketsConfig){
+  		if (j[0] === i && i !== j[1]) {
+        brackets.push(j);
+  			break;
+  		}
+      else if (i === j[0] && i === j[1]) {
+        if (brackets.includes(j)) {
+          if (j === brackets[brackets.length-1]) {
+            brackets.pop();
+          }
+          else return false;
+        }
+        else brackets.push(j);
+      }
+  		else{
+  			if (j[1] === i) {
+  				if(brackets.length !== 0) {
+  				if (i === brackets[brackets.length-1][1]){
+  					brackets.pop();
+  				}
+  			}
+  				else return false;
+  			}
+  		}
+  	}
+  }
+  	if (brackets.length === 0) return true;
+  	return false;
 }
